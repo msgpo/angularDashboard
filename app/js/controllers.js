f57fa5f3774aabe4c6eafd2d1bd5958325f7625a
Yongzhi Huang <yhuang@fastcompany.com>
2014-08-15 09:26:39 -0400

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,29 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('mainAppController',['$scope', function($scope) {
+    $scope.currentLocation = 'Latest';
+    $scope.type = 'recent';
+    $scope.limit = '40';
+    $scope.posts = [];
+  }])
+  .controller('menuPanelController', ['$scope', 'dashboardAPIService', function($scope, dashboardAPIService) {
+    $scope.menuItems = ['Create', 'Popular', 'Latest', 'Flagged'];
+
+    $scope.menuClicked = function(menuItem) {
+      $scope.currentLocation = menuItem;
+      var type = menuItem.toLowerCase();
+      var limit = '40';
+
+      dashboardAPIService.makeAPIRequest(type, limit).success(function(response){
+        $scope.posts = response.latest;
+      });
+
+    }
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+  .controller('mainDataController', function($scope, dashboardAPIService) {
+  	  dashboardAPIService.makeAPIRequest('recent', '40').success(function(response){
+        $scope.posts = response.latest;
+      });
+  });
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,37 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  factory('dashboardAPIService', function($http) {
+  	var dashboardAPI = {};
+
+
+  	dashboardAPI.makeAPIRequest = function(type, limit) {
+      var apiUrl = 'http://www.shareonfb.com/server/';      
+      if (type === 'latest') {
+      	apiUrl += 'recent.php?limit=' + limit
+      } else if (type === 'popular') {
+      	apiUrl += 'popular.php?limit=' + limit
+      } else if (type === 'reported') {
+      	apiUrl += 'banned.php?limit=' + limit
+      } else {
+        apiUrl += type + '.php?limit=' + limit;
+      }
+
+      var apiData = [];
+
+      return $http({
+        method: 'GET',
+        responseType: 'json',
+        url: apiUrl
+      });
+  	};
+
+
+  	return dashboardAPI;
+  })
+  .service('fetchDataService', ['dashboardAPIService', function(dashboardAPIService, type, limit) {
+    this.fetchData = function (type, limit) {
+      var posts = [];
+      posts = dashboardAPIService.makeAPIRequest(type,limit);
+    }
+  }]);
